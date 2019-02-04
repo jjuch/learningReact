2ec6d9e738468ab53d573419178d79f7b76bcda0
Jasper Juchem <Jasper.Juchem@UGent.be>
2019-02-04 16:00:00 +0100

    FinishAssign3

--- Assignment3/src/App.js
+++ Assignment3/src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+// import Course from './containers/Course/Course';
+import NoMatch from './component/NoMatch/NoMatch';
 
 class App extends Component {
-  render () {
-    return ( //finished task 2
+  render () { // Task 8: 27:53min
+    return (
       <div className="App">
         <ol style={{textAlign: 'left'}}>
-          <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
-          <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
-          <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
-          <li>Pass the course ID to the "Course" page and output it there</li>
-          <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
-          <li>Load the "Course" component as a nested component of "Courses"</li>
-          <li>Add a 404 error page and render it for any unknown routes</li>
+          <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links): CHECK</li>
+          <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses": CHECK</li>
+          <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now): CHECK</li>
+          <li>Pass the course ID to the "Course" page and output it there: CHECK</li>
+          <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!): CHECK</li>
+          <li>Load the "Course" component as a nested component of "Courses": CHECK</li>
+          <li>Add a 404 error page and render it for any unknown routes: CHECK</li>
           <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
         </ol>
         <nav>
@@ -28,14 +30,14 @@ class App extends Component {
             </li>
           </ul>
         </nav>
-        <Route path="/courses" component={Courses}/>
-        <Route path="/users" component={Users}/>
+        <Switch>
+          <Route path="/users" component={Users} />
+          {/* <Route path="/courses/:courseId" component={Course} /> */}
+          <Redirect from="/all-courses" to="/courses" />
+          <Route path="/courses" component={Courses} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
-            // <header>
-            //   <nav>
-            //     
-            //   </nav>
-            // </header>
     );
   }
 }
